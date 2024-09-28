0b71412237b7b724cdd8434c157ba8dbc2d1fd7d
Sokres <fieldytr@gmail.com>
2024-09-29 01:15:31 +0300

    backaup

---
+++ config/storybook/preview.js
@@ -0,0 +1,17 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecaortor'
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator'
+import { Theme } from '../../src/app/Providers/ThemProvider';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
--- src/shared/lib/classNaame/ClassName.ts
+++ src/shared/lib/classNaame/ClassName.ts
@@ -7,7 +7,7 @@ export function classNames(
     return [
         cls,
         ...Object.entries(mod)
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             .filter(([_, value]) => Boolean(value))
             .map(([key]) => key),
         ...additional.filter((el) => Boolean(el)),
